8c3fb9a8d2437d202fc3cf72d9a97b1974ec4bf7
DanyPaes <137570578+DanyPaes@users.noreply.github.com>
2024-04-10 00:44:06 -0300

    iriri
    lengo lengo engo

---
+++ aulinha2/Primeira.java
@@ -0,0 +1,24 @@
+package aulinha2;
+public class Primeira{
+
+    public static void main(String[] args) {
+
+        boolean liberacao = true;
+
+        System.out.printf("amorzinho deixa eu mimir!? eu so preciso saber o prinf na vida precisa de mais nada que isso nao\nse iver precisando eu o programando com a linguagem errada\n\n resposa esperada de dona lunna: %s", liberacao ? "si amorzinho vai dormir pra er energia pra passar o dia juninha amanhã" : "lore ipsum dolor sit amet");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

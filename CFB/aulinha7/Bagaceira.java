fb87a0392a254f076af5a8f6903eef0fbc012c89
DanyPaes <137570578+DanyPaes@users.noreply.github.com>
2024-04-17 09:38:49 -0300

    .

---
+++ CFB/aulinha7/Bagaceira.java
@@ -0,0 +1,23 @@
+package aulinha7;
+import java.util.Arrays;
+import java.util.Scanner;
+//um programa que recebe uma lisa de numeros e informa se preencheu odos os numeros ou nao, e ordena eles
+public class Bagaceira{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        final int[] lista = {23,45,3,2,6};
+        int[] luser = new int[lista.length];
+
+        for(int i = 0; i < luser.length; i++){
+            luser[i] = entrada.nextInt();
+            
+        }
+        
+
+        Arrays.sort(luser);
+        for(int i = 0; i < luser.length; i++){
+            System.out.printf("-----%d\n", luser[i]);
+        }
+    
+    }
+}

9cd77b7f0429f9f5b3a181f54efcb8304cde7bfd
DanyPaes <137570578+DanyPaes@users.noreply.github.com>
2024-05-01 09:55:32 -0300

    aulinha13

--- CFB/aulinha12/GetSet.java
+++ CFB/aulinha12/GetSet.java
@@ -1,17 +1,24 @@
 package aulinha12;
-
+//parte da aula 12 está comentada pra ser mais visivel as coisas da 13
 public class GetSet{
     public static void main(String[] args) {
         
         Orgs loud = new Orgs("LOUD", 27);
+        Orgs pain = new Orgs("PAIN", 45);
 
         loud.setNumPlayers(6);
-        int numloud = loud.getNumPlayers("LOUD");
-        String nloud = loud.getNome();
+        /*int numloud = loud.getNumPlayers("LOUD");
+        String nloud = loud.getNome();*/
+
+        System.out.print(Orgs.finalCamp(loud, pain));
+        System.out.print(Orgs.finalCamp(loud, pain));
+        System.out.print(Orgs.finalCamp(pain, loud));
+        System.out.print(Orgs.finalCamp(loud, pain));
+        System.out.print(Orgs.finalCamp(loud, pain));
 
 
 
-        System.out.printf("A organização %s tem %d players", nloud, numloud);
+        //System.out.printf("A organização %s tem %d players", nloud, numloud);
 
 
     }
--- CFB/aulinha12/Orgs.java
+++ CFB/aulinha12/Orgs.java
@@ -4,6 +4,9 @@ public class Orgs {
 
     private String nome = null;
     private int players = 0;
+    private int trofeus = 0;
+    static int split = 2;
+    static int ano = 2021;
 
     public Orgs(String name, int nplayers){
         this.nome = name;
@@ -20,6 +23,22 @@ public class Orgs {
 
     public String getNome(){
         return this.nome;
+    }
+
+    static String finalCamp(Orgs timeA, Orgs timeB){
+        System.out.printf("\nSe enfrentam na final:\n%s X %s",timeA.nome, timeB.nome);
+        timeA.trofeus++;
+        
+        System.out.println("\nO time " + timeA.nome + " venceu o campeonato e tem " + timeA.trofeus + " troféus atualmente");
+        String vencedor_split = "\nO time " + timeA.nome + " venceu o split " + split + " de " + ano +"\n---------------";
+
+        if(split > 2){
+            split = 1;
+            ano+=1;
+        }
+        split++;
+        return vencedor_split;
+        
     }
 
 

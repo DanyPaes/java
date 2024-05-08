c3622b1d64afeeb3f592517f53e08823cf9d3a7f
Lunna Cipher <lunna.devsec@gmail.com>
2024-05-08 08:13:03 -0300

    🏭 grupo de animais dinamico
    melhora o entendimento e a manutenção, animais1, animais2, animais3 força acoplamento e codigo engessado

--- CFB/aulinha14/Heranca.java
+++ CFB/aulinha14/Heranca.java
@@ -2,14 +2,19 @@ package aulinha14;
 public class Heranca {
     public static void main(String[] args) {
         
-        Zoo animal1 = new Zoo(true, true, true);
-        Zoo animal2 = new Zoo(true, true, false);
-        Zoo animal3 = new Zoo(false, true, true);
+        Zoo[] animais = new Zoo []{
+            new Zoo(true, true, true),
+            new Zoo(true, true, false),
+            new Zoo(false, true, true)
+        };
         
-        System.out.println("Animal 1: " + animal1.getCome());
-        animal1.setCome(false);
-        System.out.println("Animal 1: " + animal1.getCome());
-
+        
+        for (Zoo animal : animais) {
+            System.out.println("Animal: " + animal.getCome());
+            animal.setCome(false);
+            System.out.println("Animal: " + animal.getCome());
+        }
+        
 
     }
 }
